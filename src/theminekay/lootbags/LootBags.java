f820aa232c4b4e00dd1e191c44309f1bdf70405c
legokay11 <kaytrein@gmail.com>
2017-06-18 17:03:04 +0200

    vault economy intergration

--- src/theminekay/lootbags/LootBags.java
+++ src/theminekay/lootbags/LootBags.java
@@ -1,10 +1,39 @@
 package theminekay.lootbags;
 
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.milkbowl.vault.economy.Economy;
 
 public class LootBags extends JavaPlugin{
 
-	@Override
-	public void onEnable() {
-	}
+    private static final Logger log = Logger.getLogger("Minecraft");
+    private static Economy econ = null;
+    
+    public static Economy getEcon(){
+    	return econ;
+    }
+	
+    @Override
+    public void onEnable() {
+        if (!setupEconomy() ) {
+            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+    }
+    
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
 }
