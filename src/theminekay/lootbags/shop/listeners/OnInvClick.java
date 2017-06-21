de8a9a3c8eea4d86ad79a4915738badd43b4d812
kay houwertjes <kaytrein@gmail.com>
2017-06-21 14:42:32 +0200

    move Utils to shop package and added if states for each item in shop inventory but not for buy

--- src/theminekay/lootbags/shop/listeners/OnInvClick.java
+++ src/theminekay/lootbags/shop/listeners/OnInvClick.java
@@ -1,5 +1,8 @@
 package theminekay.lootbags.shop.listeners;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -8,6 +11,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import theminekay.lootbags.shop.inventory.InventoryManager;
+import theminekay.lootbags.shop.utils.PriceCalculator;
 
 public class OnInvClick implements Listener{
 
@@ -17,11 +21,57 @@ public class OnInvClick implements Listener{
 		Inventory inv = e.getInventory();
 		if(inv == InventoryManager.lootBagsShop){
 			ItemStack item = e.getCurrentItem();
-			ItemMeta itemMeta = item.getItemMeta();
 			if(item == InventoryManager.add1){
-				
+				InventoryManager.lootBagAmount = InventoryManager.lootBagAmount + 1;
+				e.setCancelled(true);
+				pirceLore();
+			}
+			if(item == InventoryManager.add5){
+				InventoryManager.lootBagAmount = InventoryManager.lootBagAmount + 5;
+				e.setCancelled(true);
+				pirceLore();
+			}
+			if(item == InventoryManager.add10){
+				InventoryManager.lootBagAmount = InventoryManager.lootBagAmount + 10;
+				e.setCancelled(true);
+				pirceLore();
+			}
+			if(item == InventoryManager.LootBag){
+				e.setCancelled(true);
+			}
+			if(item == InventoryManager.remove10){
+				if(InventoryManager.lootBagAmount == 1){
+					e.setCancelled(true);
+				}
+				InventoryManager.lootBagAmount = InventoryManager.lootBagAmount - 10;
+				e.setCancelled(true);
+				pirceLore();
+			}
+			if(item == InventoryManager.remove5){
+				if(InventoryManager.lootBagAmount == 1){
+					e.setCancelled(true);
+				}
+				InventoryManager.lootBagAmount = InventoryManager.lootBagAmount - 5;
+				e.setCancelled(true);
+				pirceLore();
+			}
+			if(item == InventoryManager.remove1){
+				if(InventoryManager.lootBagAmount == 1){
+					e.setCancelled(true);
+				}
+				InventoryManager.lootBagAmount = InventoryManager.lootBagAmount - 1;
+				e.setCancelled(true);
+				pirceLore();
 			}
 		}
 		
+	}
+	
+	public void pirceLore(){
+		ItemMeta meta = InventoryManager.buyMeta;
+		List<String> lore = new ArrayList<>();
+		lore.add("$" + PriceCalculator.calculatedPrice);
+		meta.setLore(lore);
+		InventoryManager.buy.setItemMeta(meta);
 	}
 }
